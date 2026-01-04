813014a1dd7e9238c83b59f7a8309061c5aac318
Andrew <andrew@Andrews-MacBook-Air.local>
2026-01-04 15:34:17 +0800

    updt

--- src/data/aboutme.ts
+++ src/data/aboutme.ts
@@ -27,7 +27,7 @@ export const aboutMe: AboutMe = {
         "I am a Ph.D. candidate in Computer Science at the School of Computing and Data Science, The University of Hong Kong (HKU), where I am fortunate to be advised by Prof. <a href='https://www.zhenqin-wu.com/'>Zhenqin (Michael) Wu</a>.<br><br> I completed my M.S. in Biology at the School of Pharmaceutical Sciences and the School of Life Sciences from Tsinghua University, and I hold a B.S. in Biotechnology from Huazhong Agricultural University.<br><br> My research interests span AI4Science, bioinformatics, and computational biology, with particular focus on spatial biology, computational genomics, and the design of biological molecules including proteins and small molecules. I am passionate about deep learning algorithms, especially generative modeling approaches. Additionally, I bring hands-on experience in experimental biology from wet lab settings. I actively collaborate with clinical and industrial partners to develop innovative AI solutions for biological and medical applications.",
     email: "yufan.liu@connect.hku.hk",
     imageUrl: "/images/lyf.jpg",
-    hoverImageUrl: "/images/lyf_hover.png",
+    hoverImageUrl: "/images/lyf_hover.PNG",
     googleScholarUrl: "https://scholar.google.com/citations?user=j9s5IIoAAAAJ&hl=en",
     githubUsername: "YAndrewL",
     linkedinUsername: "yufan-andrew-liu-506823173",

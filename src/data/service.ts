44ee0669cc79f5fbb4493ee1bdfbfe71e0db6408
Andrew <andrew@Andrews-MacBook-Air.local>
2026-03-25 08:26:50 +0800

    updt

--- src/data/service.ts
+++ src/data/service.ts
@@ -4,7 +4,7 @@ export interface Service {
 
 export const serviceData: Service[] = [
     {
-        description: "<a href='https://openreview.net/profile?id=%7EYufan_Liu7'>Conference reviewer</a> - AAAI (25'), ICLR (25', DeLTa 25', 26'), ICML (24', 25'), NeurIPS (24', 25'), AISTATS (25', 26'), ICCV (CVAMD 25')",
+        description: "Conference reviewer - AAAI (25'), ICLR (25', DeLTa 25', 26', DeLTa 26'), ICML (24', 25'), NeurIPS (24', 25', 26'), AISTATS (25', 26'), ICCV (CVAMD 25')",
     },
     {
         description: "Journal reviewer - Nature Communications",
